b5f043a1876db6868e91b3c9ba8a954224148a45
thanhnguyen98 <ngtruongthanh98@gmail.com>
2021-11-20 18:52:29 +0700

    practice with Route, Link

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,9 +1,19 @@
 // our-domain.com/news/
-
-import React from 'react';
+import Link from 'next/link';
+import React, { Fragment } from 'react';
 
 const NewsPage = () => {
-  return <h1>The news page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/news-1">News 1</Link>
+        </li>
+        <li>News 2</li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default NewsPage;
