79c05de35b7f98dada781438b63b64a4ff162bfd
thanhnguyen98 <ngtruongthanh98@gmail.com>
2021-11-20 17:24:36 +0700

    Extracting dynamic route data

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,8 +1,15 @@
 // our-domain.com/news/something-important
 
 import React from 'react';
+import { useRouter } from 'next/router';
 
 const DetailPage = () => {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  // send a request to the server to get the news
+
   return <h1>The news page</h1>;
 };
 
